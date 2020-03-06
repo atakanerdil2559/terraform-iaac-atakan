17149d7727113ee2687aa01dc126fc05607385cd
Cloud User <centos@ip-172-31-25-126.eu-west-2.compute.internal>
2020-03-06 01:59:24 +0000

    added

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-0a887e401f7654935"
   instance_type = "t2.micro"
-  key_name      ="${aws_key_pair.deployer-key.key_name }"
+  key_name      ="${aws_key_pair.deployer.key_name }"
 
 }
