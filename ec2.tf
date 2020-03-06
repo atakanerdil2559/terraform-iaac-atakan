d7341ac78f1c81f21a0a423a2c1fe3804dcea83e
Cloud User <centos@ip-172-31-25-126.eu-west-2.compute.internal>
2020-03-06 01:49:46 +0000

    ++

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,4 @@
 resource "aws_instance" "web" {
-  count = "10"
   ami           = "ami-0a887e401f7654935"
   instance_type = "t2.micro"
 
---
+++ key_pair.tf
@@ -0,0 +1,3 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+ public_key = "${file("~/.ssh/id_rsa.pub")}"
