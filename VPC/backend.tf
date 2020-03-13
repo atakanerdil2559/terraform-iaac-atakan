ce59478cdc56d7e83f86793e5752868624e45552
Cloud User <centos@ip-172-31-21-160.eu-west-2.compute.internal>
2020-03-13 21:53:43 +0000

    added

--- VPC/backend.tf
+++ VPC/backend.tf
@@ -1,7 +1,7 @@
 terraform {
 backend "s3" {
 bucket = "terraform-class-atakan"
-key = "tower/us-west-2/tools/oregon/tower.tfstate"
-region = "us-west-2"
+key = "tower/us-east-1/tools/Hong Kong/tower.tfstate"
+region = "us-east-1"
   }
 }
